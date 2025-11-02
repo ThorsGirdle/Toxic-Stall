d3f614ec44d1d8b3411f10ba1ec16a72bcbc1b7d
ThorsGirdle <stephenwhiteebc@gmail.com>
2025-11-01 19:45:04 -0500

    Amoongus changes

--- src/pokemon/0590_foongus_line.lua
+++ src/pokemon/0590_foongus_line.lua
@@ -48,8 +48,7 @@ local amoonguss = {
 	config = {extra = {Xmult_mod = 0.25, mult = 0, mult_mod = 5}},
 	loc_vars = function(self, info_queue, card)
 		type_tooltip(self, info_queue, card)
-		info_queue[#info_queue+1] = G.P_CENTERS.m_stall_toxic
-		
+		info_queue[#info_queue+1] = G.P_CENTERS.c_stall_blacksludge
 	  return {vars = {card.ability.extra.Xmult_mod, card.ability.extra.mult, card.ability.extra.mult_mod}}
 	end,
 	rarity = "poke_safari", --Safari
@@ -64,6 +63,15 @@ local amoonguss = {
 	blueprint_compat = true,
 	eternal_compat = true,
 	
+	add_to_deck = function(self, card, from_debuff)
+    if not from_debuff and #G.consumeables.cards + G.GAME.consumeable_buffer < G.consumeables.config.card_limit then
+      local _card = create_card('Item', G.consumeables, nil, nil, nil, nil, 'c_stall_blacksludge')
+      _card:add_to_deck()
+      G.consumeables:emplace(_card)
+      card_eval_status_text(_card, 'extra', nil, nil, nil, {message = localize('poke_plus_pokeitem'), colour = G.C.FILTER})
+			return true
+		end
+	end,
 	calculate = function(self, card, context)
 		if context.cardarea == G.jokers and not context.blueprint then
 			if context.setting_blind and not card.getting_sliced then
@@ -73,6 +81,7 @@ local amoonguss = {
 					extra = { message = 'Effect Spore', colour = G.C.MULT },
 				}
 			end
+			--[[
 			if context.joker_main and card.ability.extra.mult > 0 then
 				return{
 					message = localize{type = 'variable', key = 'a_mult', vars = {card.ability.extra.mult}}, 
@@ -87,7 +96,7 @@ local amoonguss = {
 				return {
 						extra = { message = localize('k_upgrade_ex'), colour = G.C.MULT}
 						}
-			end	
+			end --]]	
 		end
 	end,
 }
