029edc3698bd48e787f9ca027c356e2ac4d42e9a
ThorsGirdle <stephenwhiteebc@gmail.com>
2025-12-21 03:42:03 -0600

    Update popplio line evo req

--- src/pokemon/0728_popplio_line.lua
+++ src/pokemon/0728_popplio_line.lua
@@ -1,7 +1,7 @@
 local popplio = {
 	name = "popplio",
 	pos = {x = 0, y = 0},
-	config = {extra = {mult = 0, mult_mod = 1, hands = 1, poker_hand = "High Card"}, evo_rqmt = 12},
+	config = {extra = {mult = 0, mult_mod = 1, hands = 1, poker_hand = "High Card"}, evo_rqmt = 8},
 	loc_vars = function(self, info_queue, card)
 		type_tooltip(self, info_queue, card)
 		local abbr = card.ability.extra
@@ -75,7 +75,7 @@ local popplio = {
 local brionne = {
 	name = "brionne",
 	pos = {x = 0, y = 0},
-	config = {extra = {mult = 0, mult_mod = 2, hands = 1, poker_hand = "High Card"}, evo_rqmt = 30},
+	config = {extra = {mult = 0, mult_mod = 2, hands = 1, poker_hand = "High Card"}, evo_rqmt = 20},
 	loc_vars = function(self, info_queue, card)
 		type_tooltip(self, info_queue, card)
 		local abbr = card.ability.extra
@@ -243,4 +243,5 @@ local primarina = {
 return {name = "Popplio Line", 
 enabled = stall_config.Popplio or false,
 list = {popplio, brionne, primarina}
+
 }
