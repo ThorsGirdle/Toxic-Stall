cc9775df2e373cbd8b7b0287385b8fe7eeb689fe
ThorsGirdle <stephenwhiteebc@gmail.com>
2025-12-22 00:22:46 -0600

    rellor line text fix

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -663,16 +663,16 @@ return {
 			j_stall_rellor = {
 				name = 'Rellor',      
 				text = {
-					"{C:mult}+#1#{} per {C:item}Item{} card",
+					"{C:mult}+#1#{} Mult per {C:item}Item{} card",
 					"used this run",
-					"{C:inactive}(Currently {C:mult}+#2#{C:inactive} Mult )",
+					"{C:inactive}(Currently {C:mult}+#2#{C:inactive} Mult)",
 					"{C:inactive,s:0.8}(Evolves after using {C:attention,s:0.8}#3#{C:inactive,s:0.8} Items)",
 				}
 			},
 			j_stall_rabsca = {
 				name = 'Rabsca',      
 				text = {
-					"{C:mult}+#1#{} per {C:item}Item{} and ",
+					"{C:mult}+#1#{} Mult per {C:item}Item{} and ",
 					"{C:tarot}Tarot{} card used this run",
 					"{C:inactive}(Currently {C:mult}+#2#{C:inactive} Mult)",
 				} 
