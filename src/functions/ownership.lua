d8f7f49017b93dd2aad76cde0550c3b519e504dd
ThorsGirdle <stephenwhiteebc@gmail.com>
2026-09-23 00:27:53 -0500

    Add files via upload

--- src/functions/ownership.lua
+++ src/functions/ownership.lua
@@ -74,17 +74,7 @@ SMODS.Enhancement:take_ownership("poke_seed", {
 
 }, true)
 
-if (SMODS.Mods["Agarmons"] or {}).can_load then
-	SMODS.Enhancement:take_ownership("agar_combee", {
-		calculate = function(self, card, context)
-			if context.change_rank and context.new_rank == 12
-					and (context.old_rank == 11 or context.old_rank == 13) and not SMODS.has_enhancement(context.other_card, 'm_stall_vestige') then
-				card.ability.extra.queens_promoted = card.ability.extra.queens_promoted + 1
-			end
-			return pokermon.scaling_evo(self, card, context, 'j_agar_vespiquen', card.ability.extra.queens_promoted, 1)
-		end
-	}, true)
-end
+
 
 return {
   key = "STALL_OWNERSHIP",
